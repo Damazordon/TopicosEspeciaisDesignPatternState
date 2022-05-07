a6e5ee4cf6eae15fd46a0aa03f98b57483aa2f2c
Gabriel <gabrielgabridma@gmail.com>
2022-05-07 12:11:44 -0300

    Criação das primeiras classes para testes, validação de nomes e import

---
+++ Interface.py
@@ -0,0 +1,16 @@
+#Interface do jogo
+import _tkinter
+
+class interface():
+    pass
+
+class começar():
+    pass
+
+class opções():
+    pass
+
+class sair():
+    pass
+
+print("teste de import concluido")
